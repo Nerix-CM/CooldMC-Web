3ad27bd2729aa457528d7e91f7b18f12c9de570a
_Nerix <andrew.sobakin2011@gmail.com>
2026-05-02 20:29:12 +0300

    Update players.js

--- players.js
+++ players.js
@@ -7,5 +7,6 @@ const playersData = [
     { name: "Skortek", role: null, joinDate: "2025-09-13" },
     { name: "Ded7s", role: null, joinDate: "2025-12-26" },
     { name: "C_A_D_1_S_T", role: null, joinDate: "2026-02-04" },
-    { name: "_Zora_", role: null, joinDate: "2025-11-25"}
+    { name: "_Zora_", role: null, joinDate: "2025-11-25"}, 
+    { name: "_Enflis_", role: null, joinDate: "2026-25-04"}
 ];
