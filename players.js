3bb8f35054eb5f2650440bf3ac2725442e539aa5
_Nerix <andrew.sobakin2011@gmail.com>
2026-05-17 22:59:59 +0300

    Update players.js

--- players.js
+++ players.js
@@ -2,31 +2,31 @@
 
 const playersData = [
     // ГЛ.Администратор (&c)
-    { name: "_Nerix", role: "gl_admin", roleText: "ГЛ.Администратор", joinDate: "2025-09-11" },
+    { name: "_Nerix", role: "gl_admin", joinDate: "2025-09-11" },
     
     // Администратор (&c)
-    // { name: "ПримерАдмина", role: "admin", roleText: "Администратор", joinDate: "2025-09-11" },
+    // { name: "ПримерАдмина", role: "admin", joinDate: "2025-09-11" },
     
     // ГЛ.Модератор (&9)
-    { name: "Tarkos0", role: "gl_moderator", roleText: "ГЛ.Модератор", joinDate: "2025-09-12" },
+    { name: "Tarkos0", role: "gl_moderator", joinDate: "2025-09-12" },
     
     // СТ.Модератор (&9)
-    // { name: "ПримерСтМодератора", role: "st_moderator", roleText: "СТ.Модератор", joinDate: "2025-09-12" },
+    // { name: "ПримерСтМодератора", role: "st_moderator", joinDate: "2025-09-12" },
     
     // Модератор (&9)
-    // { name: "ПримерМодератора", role: "moderator", roleText: "Модератор", joinDate: "2025-09-12" },
+    // { name: "ПримерМодератора", role: "moderator", joinDate: "2025-09-12" },
     
     // ГЛ.Хелпер (&6)
-    // { name: "ПримерГлХелпера", role: "gl_helper", roleText: "ГЛ.Хелпер", joinDate: "2025-10-01" },
+    // { name: "ПримерГлХелпера", role: "gl_helper", joinDate: "2025-10-01" },
     
     // СТ.Хелпер (&6)
-    // { name: "ПримерСтХелпера", role: "st_helper", roleText: "СТ.Хелпер", joinDate: "2025-10-01" },
+    // { name: "ПримерСтХелпера", role: "st_helper", joinDate: "2025-10-01" },
     
     // Хелпер (&6)
-    // { name: "ПримерХелпера", role: "helper", roleText: "Хелпер", joinDate: "2025-10-01" },
+    // { name: "ПримерХелпера", role: "helper", joinDate: "2025-10-01" },
     
     // Бета-тестер (&e)
-    // { name: "ПримерБетаТестера", role: "beta", roleText: "Бета-тестер", joinDate: "2026-05-05" },
+    // { name: "ПримерБетаТестера", role: "beta", joinDate: "2026-05-05" },
     
     // Обычные игроки (без роли)
     { name: "Okosan2", role: null, joinDate: "2025-12-20" },
